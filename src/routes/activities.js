02fa23c44f9ca7c9a5d9ed13a92c85d083d5a601
jessasesh <jessica.dison@yahoo.com>
2024-12-17 19:39:59 -0600

    Swagger documentation

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -3,6 +3,42 @@ const router = express.Router();
 const Activity = require('../models/Activity');
 
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Countries
+ *     description: Endpoints for country data
+ */
+
+/**
+ * @swagger
+ * /api/countries:
+ *   get:
+ *     tags: [Countries]
+ *     summary: Get all countries
+ *     description: Returns a list of all countries with their attributes.
+ *     responses:
+ *       200:
+ *         description: Successfully fetched countries
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                     example: Canada
+ *                   healthcare_structure:
+ *                     type: string
+ *                     example: Universal
+ *                   obesity_rating:
+ *                     type: number
+ *                     example: 27.32
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/', async (req, res) => {
     try {
         const activities = await Activity.find();
