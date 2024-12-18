2944831835c349f6194e012b655383db13765809
jessasesh <jessica.dison@yahoo.com>
2024-12-17 19:38:51 -0600

    Update for swagger implementation and endpoint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const path = require('path');
 const connectDB = require('./config/db');
 const logger = require('./config/logger');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 
 dotenv.config();
@@ -14,11 +16,31 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use((req, res, next) => {
     logger.info(`Incoming ${req.method} request to ${req.url}`);
     next();
 });
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Workouts API',
+            version: '1.0.0',
+            description: 'API Documentation for the Workouts API',
+            contact: {
+                name: 'API Support',
+            },
+            servers: [
+                { url: 'http://localhost:5000' }
+            ]
+        }
+    },
+    apis: [path.join(__dirname, './routes/*.js')],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/', (req, res) => {
     logger.info('Test route hit: GET /');
